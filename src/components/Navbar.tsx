f3272c6448a119caa75ffe4d3522c6e73c02d2ae
tyler <ruiztyler24@gmail.com>
2025-06-15 12:01:34 -0400

    fix(Navbar): add missing href attributes to links for navigation feat(diary): create diary stats page component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,11 @@ function Navbar() {
     return (
         <div className="navbar bg-white shadow-xl">
             <div className="flex-1">
-                <a className="btn btn-ghost text-2xl">FitLog</a>
+                <a className="btn btn-ghost text-2xl" href="/">FitLog</a>
             </div>
             <div className="flex gap-2 ">
                 <ul className="menu menu-horizontal px-1 text-xl">
-                    <li><a className="btn btn-ghost ">Start Logging!</a></li>
+                    <li><a href="/diary" className="btn btn-ghost ">Start Logging!</a></li>
                     <li><a className="btn btn-ghost">Friends</a></li>
                     <li><a className="btn btn-ghost">Join</a></li>
                 </ul>
