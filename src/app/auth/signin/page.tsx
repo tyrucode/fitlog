1035edf8d346ac17dfd00c67b583359425857ad5
ruizt <ruiztyler24@gmail.com>
2025-06-16 18:54:52 -0400

    feat: implement sign-in and sign-up forms with validation and improved layout

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+'use client';
 
 function page() {
+
+    const signIn = () => {
+        console.log('sign in!');
+    };
+
     return (
-        <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20)]"><h1>welcome to signin page</h1></div>
+        <div className="flex flex-col items-center justify-center p-8 gap-16 sm:p-20">
+            <h1 className="text-2xl font-semibold text-center">Sign In!</h1>
+            <div className="border border-black rounded p-6 w-full max-w-md bg-white shadow-md">
+                <form
+                    className="flex flex-col gap-4"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        signIn();
+                    }}
+                >
+
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        required
+                        placeholder="fakeEmail@email.com"
+                        className="bg-neutral-300 border border-black rounded px-3 py-2 text-black focus:outline-none focus:ring-2 focus:ring-black"
+                    />
+
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        required
+                        placeholder="Password"
+                        minLength={8}
+                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                        title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                        className="bg-neutral-300 border border-black rounded px-3 py-2 text-black focus:outline-none focus:ring-2 focus:ring-black"
+                    />
+                    <button type="submit" className="btn btn-ghost">
+                        Sign In!
+                    </button>
+                    <a className="btn btn-ghost" href="/auth/signup">Dont have an account? Sign Up!</a>
+                </form>
+            </div>
+        </div>
     )
 }
 
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,29 +1,68 @@
-import React from 'react'
+'use client';
 
-function page() {
+function Page() {
+
+    const signUp = () => {
+        console.log('sign up!');
+    };
+
     return (
         <div className="flex flex-col items-center justify-center p-8 gap-16 sm:p-20">
             <h1 className="text-2xl font-semibold text-center">Welcome to Sign Up Page</h1>
-
             <div className="border border-black rounded p-6 w-full max-w-md bg-white shadow-md">
-                <div className="flex flex-col gap-4">
+                <form
+                    className="flex flex-col gap-4"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        signUp();
+                    }}
+                >
+                    <label htmlFor="fname">First Name:</label>
                     <input
                         type="text"
+                        id="fname"
+                        required
+                        placeholder="First Name"
                         className="bg-neutral-300 border border-black rounded px-3 py-2 text-black focus:outline-none focus:ring-2 focus:ring-black"
-                        placeholder="fakeEmail@email.com"
                     />
+
+                    <label htmlFor="lname">Last Name:</label>
+                    <input
+                        type="text"
+                        id="lname"
+                        required
+                        placeholder="Last Name"
+                        className="bg-neutral-300 border border-black rounded px-3 py-2 text-black focus:outline-none focus:ring-2 focus:ring-black"
+                    />
+
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        required
+                        placeholder="fakeEmail@email.com"
+                        className="bg-neutral-300 border border-black rounded px-3 py-2 text-black focus:outline-none focus:ring-2 focus:ring-black"
+                    />
+
+                    <label htmlFor="password">Password:</label>
                     <input
                         type="password"
+                        id="password"
+                        required
+                        placeholder="Password"
+                        minLength={8}
+                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                        title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
                         className="bg-neutral-300 border border-black rounded px-3 py-2 text-black focus:outline-none focus:ring-2 focus:ring-black"
-                        placeholder="password123"
                     />
-                    <button className="btn btn-ghost ">Create Account</button>
-                    <button className="btn btn-ghost ">Already have an account? Sign In</button>
-                </div>
+                    <button type="submit" className="btn btn-ghost">
+                        Create Account
+                    </button>
+                    <a className="btn btn-ghost" href="/auth/signin">Already have an account? Sign In!</a>
+                </form>
             </div>
         </div>
     );
-
 }
 
-export default page
+export default Page;
