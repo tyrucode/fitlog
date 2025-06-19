17a5e2145806afbbaa13293f8c1647f93b52f7e0
ruizt <ruiztyler24@gmail.com>
2025-06-19 17:30:13 -0400

    feat: update Navbar to display dynamic dashboard text based on user authentication state

--- src/app/diary/page.tsx
+++ src/app/diary/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
     return (
         <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20)]">
             <h1>YOUR STATS</h1>
-            <p>Welcome, {user.email}!</p>;
+            <p>Welcome, {user.email}!</p>
         </div>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,11 @@
-
+'use client';
+//context
+import { useUser } from '@/context/UserContext';
+import { useState } from 'react';
 
 function Navbar() {
+    const { user } = useUser();
+    const dashText = !user ? "Dashboard" : "Start Logging!";
     return (
         <div className="navbar bg-white shadow-xl">
             <div className="flex-1">
@@ -8,7 +13,7 @@ function Navbar() {
             </div>
             <div className="flex gap-2 ">
                 <ul className="menu menu-horizontal px-1 text-xl">
-                    <li><a href="/diary" className="btn btn-ghost ">Start Logging!</a></li>
+                    <li><a href="/diary" className="btn btn-ghost ">{dashText}</a></li>
                     <li><a className="btn btn-ghost">Friends</a></li>
                     <li><a className="btn btn-ghost" href="/auth/signup">Join</a></li>
                 </ul>
