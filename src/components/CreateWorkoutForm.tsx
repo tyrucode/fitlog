088365d05022a25fa8af3a8e2aa9dd7788d0f129
ruizt <ruiztyler24@gmail.com>
2025-06-22 16:26:59 -0400

    feat: enhance CreateWorkoutForm layout with improved structure and styling

--- src/components/CreateWorkoutForm.tsx
+++ src/components/CreateWorkoutForm.tsx
@@ -1,7 +1,14 @@
 
 function CreateWorkoutForm() {
     return (
-        <div><h1>Create a workout</h1></div>
+        <div>
+            <h1>Create a workout</h1>
+            <div className="border border-black rounded p-6 w-full max-w-md bg-white shadow-md">
+                <div className="flex flex-col gap-4">
+                    <p>Start by creating your workout</p>
+                </div>
+            </div>
+        </div >
     )
 }
 
