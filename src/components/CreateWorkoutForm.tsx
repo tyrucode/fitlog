941f8fbcf352814cff6fa43cba5f63a25f3370f7
ruizt <ruiztyler24@gmail.com>
2025-06-23 21:22:35 -0400

    feat: implement form submission handling in CreateWorkoutForm component

--- src/components/CreateWorkoutForm.tsx
+++ src/components/CreateWorkoutForm.tsx
@@ -1,14 +1,27 @@
+"use client"
+import React, { useState } from "react"
+import { createWorkout, type NewWorkout } from "@/lib/workoutOperations"
+
 
 function CreateWorkoutForm() {
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log('form submitted');
+
+    }
+
+
     return (
-        <div>
-            <h1>Create a workout</h1>
+        <>
+            <h2 className="text-xl font-semibold mb-4">Create a Workout</h2>
             <div className="border border-black rounded p-6 w-full max-w-md bg-white shadow-md">
                 <div className="flex flex-col gap-4">
                     <p>Start by creating your workout</p>
+                    <form onSubmit={handleSubmit}></form>
                 </div>
             </div>
-        </div >
+        </>
     )
 }
 
