244cd3df24a8257efd44e56f434439093afa1913
ruizt <ruiztyler24@gmail.com>
2025-06-28 18:31:13 -0400

    feat: add additional information about workout logging and social features in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
     <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20)]">
       <h1>Welcome to FitLog!</h1>
       <p>To use this app sign up, and log all your workouts!</p>
+      <p>Fitlog is great for logging workouts and getting visual indicators of your workout times</p>
+      <p>you can also visit your friends pages and see how their workouts are going as well!</p>
 
     </div>
   );
