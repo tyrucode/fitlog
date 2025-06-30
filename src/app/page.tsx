053ad516c7976136fb118318b88033d89ae4e137
ruizt <ruiztyler24@gmail.com>
2025-06-30 18:51:58 -0400

    feat: restructure Home component layout for improved readability and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,21 @@
 export default function Home() {
   return (
     <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20)]">
-      <h1>Welcome to FitLog!</h1>
-      <p>To use this app sign up, and log all your workouts!</p>
-      <p>Fitlog is great for logging workouts and getting visual indicators of your workout times</p>
-      <p>you can also visit your friends pages and see how their workouts are going as well!</p>
-
-    </div>
+      <div className="min-h-screen bg-neutral-100 p-4 sm:p-8">
+        <div className="max-w-7xl mx-auto mb-8">
+          <div className="bg-white border border-black rounded-lg p-6 shadow-xl">
+            <div className="text-center">
+              <h1 className='text-4xl font-bold mb-2 tracking-tight'>Welcome to FitLog!</h1>
+              <div className="h-1 w-24 bg-black mx-auto mb-4"></div>
+              <p className='text-xl text-neutral-600'>
+                <p>To use this app sign up, and log all your workouts!</p>
+                <p>Fitlog is great for logging workouts and getting visual indicators of your workout times</p>
+                <p>you can also visit your friends pages and see how their workouts are going as well!</p>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div >
+    </div >
   );
 }
