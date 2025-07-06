b874e118bbd7250834a81405d4024cb505c80359
ruizt <ruiztyler24@gmail.com>
2025-07-06 19:37:55 -0400

    feat: update Not Found page layout and improve structure

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,14 @@
 function notfound() {
     return (
         <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20)]">
-            <h1>PAGE NOT FOUND!</h1>
 
+            <div className='flex flex-col items-center min-h-screen p-8 gap-16 sm:p-20'>
+                <div className="bg-white border flex flex-col border-black rounded-lg p-8 shadow-xl">
+                    <h2 className="text-2xl font-bold mb-4">PAGE NOT FOUND</h2>
+                    <div className="animate-pulse text-xl mb-4 font-semibold text-red-600">Returning home in { } seconds</div>
+                </div>
+            </div>
+            );
         </div>
     )
 }
