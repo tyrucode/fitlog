d2d4dd13ab50789c6bfad0753be724c2514751b2
ruizt <ruiztyler24@gmail.com>
2025-07-06 19:40:56 -0400

    feat: implement countdown timer and navigation for Not Found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,35 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useRouter } from "next/router";
 
 function notfound() {
+    // state for the not found page
+    const [seconds, setSeconds] = useState(5);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setSeconds((prevSeconds) => { // take the timer and  when it hits 1 nav the user to the home page
+                if (prevSeconds === 1) {
+                    clearInterval(timer); // stop the timer
+                    navigate("/"); // send user home
+                    return 0;
+                }
+                return prevSeconds - 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(timer); // cleanup
+    }, [navigate]);
+
     return (
         <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20)]">
-
             <div className='flex flex-col items-center min-h-screen p-8 gap-16 sm:p-20'>
                 <div className="bg-white border flex flex-col border-black rounded-lg p-8 shadow-xl">
                     <h2 className="text-2xl font-bold mb-4">PAGE NOT FOUND</h2>
                     <div className="animate-pulse text-xl mb-4 font-semibold text-red-600">Returning home in { } seconds</div>
                 </div>
             </div>
-            );
         </div>
     )
 }
